87d6f84fc3fe634b15cb8f2103ed8c7bcff87e7f
mpique <piqferma@gmail.com>
2024-05-02 17:47:09 +0200

    Feat: Copy response to body button

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ $(document).ready(function() {
         var host = $('#hostInput').val();
         var port = $('#portInput').val();
         var method = $('#methodSelect').val();
-        var body = $('#bodyInput').val(); // Get the body content
+        var body = $('#bodyInput').val();
 
         $.ajax({
             url: '/' + method.toLowerCase() + '_interface_info',
             type: method,
-            data: JSON.stringify({host: host, port: port, body: body}), // Include body in the data
+            data: JSON.stringify({host: host, port: port, body: body}), 
             contentType: 'application/json',
             success: function(data) {
                 $('#interfaceInfo').html(JSON.stringify(data, null, 2));
             }
         });
+    });
+
+    // Button to copy the response to the Body input
+    $('#copyResponseButton').click(function() {
+        var responseData = $('#interfaceInfo').text();
+        $('#bodyInput').val(responseData);
     });
 });
