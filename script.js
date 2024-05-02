b305f7855bf1abc28481d9f80a4d62d13f48b12d
mpique <piqferma@gmail.com>
2024-05-02 17:28:53 +0200

    Feat: Add Body option

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
         var host = $('#hostInput').val();
         var port = $('#portInput').val();
         var method = $('#methodSelect').val();
+        var body = $('#bodyInput').val(); // Get the body content
 
         $.ajax({
             url: '/' + method.toLowerCase() + '_interface_info',
             type: method,
-            data: JSON.stringify({host: host, port: port}),
+            data: JSON.stringify({host: host, port: port, body: body}), // Include body in the data
             contentType: 'application/json',
             success: function(data) {
                 $('#interfaceInfo').html(JSON.stringify(data, null, 2));
